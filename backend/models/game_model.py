620051ff02ae7d7fa9454cb6f7cb5a4f3361d983
killrr <64957160+cnvr76@users.noreply.github.com>
2026-03-21 16:04:06 +0100

    fix/: upgraded models

--- backend/models/game_model.py
+++ backend/models/game_model.py
@@ -1,9 +1,15 @@
 import uuid
-from sqlalchemy import Column, Text, Enum as SQLEnum
+from sqlalchemy import Column, Text, Enum as SQLEnum, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 from config.database_config import Base
 from enum import Enum
+
+
+class GameStatuses(str, Enum):
+    PRIVATE = "private"
+    PENDING = "pending"
+    PUBLISHED = "published"
 
 
 class AgeGroups(str, Enum):
@@ -11,18 +17,21 @@ class AgeGroups(str, Enum):
     MIDDLE = "5-6 rokov"
 
 
-age_groups_enum: SQLEnum = SQLEnum(AgeGroups, name="age_group", values_callable=lambda items: [enum.value for enum in items])
+game_statuses_enum: SQLEnum = SQLEnum(GameStatuses, name="statuses", values_callable=lambda items: [enum.value for enum in items])
 
 
 class Game(Base):
     __tablename__ = "games"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    author_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="SET NULL"), nullable=True)
     name = Column(Text, nullable=False)
     description = Column(Text)
     preview_image_url = Column(Text)
-    age_group = Column(age_groups_enum, nullable=False)
+    age_group = Column(Text, nullable=False)
     game_type = Column(Text, nullable=False)
+    status = Column(game_statuses_enum, nullable=False, default=GameStatuses.PRIVATE.value)
     config_data = Column(JSONB, nullable=False)
 
     activities = relationship("Activity", back_populates="game")
+    user = relationship("User", back_populates="games")
--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,16 +1,38 @@
 import uuid
-from sqlalchemy import Column, Text, DateTime, func
+from sqlalchemy import Column, Text, DateTime, func, Enum as SQLEnum, Table, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from config.database_config import Base
+from enum import Enum
+
+
+class Roles(str, Enum):
+    PARENT = "parent"
+    THERAPIST = "therapist"
+    ADMIN = "admin"
+    
+
+roles_eum: SQLEnum = SQLEnum(Roles, name="roles", values_callable=lambda items: [enum.value for enum in items])
+
+
+favorites_table = Table(
+    "favorites",
+    Base.metadata,
+    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), primary_key=True),
+    Column("game_id", UUID(as_uuid=True), ForeignKey("games.id", ondelete="CASCADE"), primary_key=True)
+)
 
 
 class User(Base):
     __tablename__ = "users"
-
+    
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_session_id = Column(UUID(as_uuid=True), unique=True, nullable=False)
-    username = Column(Text, nullable=True)
+    username = Column(Text, nullable=False)
+    email = Column(Text, nullable=False, unique=True)
+    password_hash = Column(Text, nullable=False)
+    role = Column(roles_eum, nullable=False, default=Roles.PARENT.value)
     created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
-
+    
     activities = relationship("Activity", back_populates="user")
+    games = relationship("Game", back_populates="user")
+    favorites = relationship("Game", secondary=favorites_table)
